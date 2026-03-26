d45b8e8ce8d8f68de2bd849f77ef5977d11f5c4f
Ruslan Melnyk <ruslan_melnyk@ukr.net>
2026-03-26 19:39:22 +0200

    ++

---
+++ commands.sh
@@ -0,0 +1,19 @@
+# встановлення pre-commit
+pip install pre-commit
+pre-commit install
+
+# запуск
+pre-commit run --all-files
+pre-commit run --files [file_path]
+
+# подивитися на всі гілки
+git branch
+
+# створити нову гілку
+git branch [branch_name]
+
+# перейти на гілку
+git checkout [branch_name]
+
+# завантаження змін з github
+git pull origin [branch_name]
