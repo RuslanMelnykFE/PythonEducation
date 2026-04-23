6987dcc177f8c092865cea8580eb630b729a5a99
Ruslan Melnyk <ruslan_melnyk@ukr.net>
2026-04-23 20:37:24 +0300

    end task 2 module_13_1

--- module_13/module_13_1.py
+++ module_13/module_13_1.py
@@ -1,4 +1,5 @@
 import json
+from typing import Optional, List
 
 
 # Завдання 1
@@ -16,9 +17,12 @@ FILENAME = "users.json"
 
 
 def get_data_users(filename: str = FILENAME) -> dict[str, str] | None:
-    with open(filename, "r", encoding="utf-8") as file:
-        data = json.load(file)
-        return data
+    try:
+        with open(filename, "r", encoding="utf-8") as file:
+            return json.load(file)
+    except FileNotFoundError:
+        print(f"File {filename} not found.")
+        return None
 
 
 def save_users(
@@ -125,6 +129,73 @@ def main():
             break
         else:
             print("Невірний вибір!")
+
+
+# Завдання 2
+# Створіть клас Cart
+# Атрибути:
+#  user – ім’я користувача
+#  items – список товарів
+#  total – загальна ціна
+# Методи:
+#  add(item, price) – добавити товар у кошик
+#  delete(item, price) – видалити товар з кошика
+#  info() – вивести інформацію про кошик
+#  save(fiename) – зберегти дані у файл(за
+# замовчуванням cart.json)
+#  load(fiename) – завантажити дані з файла(за
+# замовчуванням cart.json)
+
+
+class Cart:
+    def __init__(
+        self, user: str, items: Optional[List[str]] = None, total: float = 0
+    ) -> None:
+        self._user = user
+        self._total = total
+
+        if items is None:
+            items = []
+        else:
+            self._items = items
+
+    def add_item(self, item: str, price: float) -> None:
+        if item not in self._items:
+            self._items.append(item)
+
+        self._total += price
+
+    def delete_item(self, item: str, price: float) -> None:
+        if item not in self._items:
+            print("Такого товару не має в кошику")
+            return
+
+        self._items.remove(item)
+        self._total -= price
+
+    def show_info(self) -> None:
+        print(
+            f"В кошику є товари:\n"
+            f"{', '.join(self._items)}\n"
+            f"на суму: {self._total}"
+        )
+
+    def save(self, filename: str = "cart.json") -> None:
+        data = {
+            "user": self._user,
+            "products": self._items,
+            "total": self._total,
+        }
+
+        with open(filename, "w", encoding="utf-8") as file:
+            json.dump(data, file)
+
+    def load(self, filename: str = "cart.json") -> None:
+        with open(filename, "r", encoding="utf-8") as file:
+            data = json.load(file)
+            self._user = data["user"]
+            self._items = data["products"]
+            self._total = data["total"]
 
 
 if __name__ == "__main__":
