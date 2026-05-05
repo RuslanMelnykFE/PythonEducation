14a53dc00ef830d5bd5e89271fc095f4b6da4e1d
Ruslan Melnyk <ruslan_melnyk@ukr.net>
2026-05-05 21:16:47 +0300

    end tasks 1-3 module_14_2

--- module_14/lesson_14_2.py
+++ module_14/lesson_14_2.py
@@ -1,3 +1,5 @@
+import json
+from typing import List
 from fastapi import FastAPI
 from pydantic import BaseModel
 
@@ -76,3 +78,87 @@ class User(BaseModel):
 @app.post("/hello")
 def save_message(name: str) -> Message:
     return Message(message=f"привіт, {name}")
+
+
+# Завдання 4
+# Напишіть сервер для симуляції роботи бібліотеки.
+# Дані про книги знаходяться у файлі books.json
+# Напишіть модель на pydentic для книги з такими
+# даними:
+# ● id
+# ● title
+# ● author
+# ● year
+# ● pages
+# Функціонал:
+# 1. Отримання всіх книг
+# ○ шлях – books
+# ○ метод – GET
+# 2. Отримання даних за ID книги
+# ○ шлях – books/{book_id}
+# ○ метод – GET
+# 3. Додавання нової книги
+# ○ шлях – books
+# ○ метод – POST
+# 4. Видалення книги за ID
+# ○ шлях – books/{book_id}
+# ○ метод – DELETE
+
+
+class Book(BaseModel):
+    id: str
+    title: str
+    author: str
+    year: int
+    pages: int
+
+
+@app.get("/books")
+def get_books() -> List[Book]:
+    try:
+        with open("books.json", "r", encoding="utf-8") as file:
+            return json.load(file)
+
+    except FileNotFoundError:
+        print("File not found")
+        return []
+
+
+@app.get("/books/{book_id}")
+def get_book(book_id: str):
+    try:
+        with open("books.json", "r", encoding="utf-8") as file:
+            books = json.load(file)
+
+            for book in books:
+                if book["id"] == book_id:
+                    return book
+
+    except FileNotFoundError:
+        print("File not found")
+        return {}
+
+
+@app.post("/books")
+def save_book(book: Book) -> Message:
+    with open("books.json", "r", encoding="utf-8") as file:
+        books = json.load(file)
+
+    books.append(book.model_dump())
+
+    with open("books.json", "w", encoding="utf-8") as file:
+        json.dump(books, file)
+
+    return Message(message="Book added successfully")
+
+
+@app.delete("/books/{book_id}")
+def delete_book(book_id: str) -> Message:
+    with open("books.json", "r", encoding="utf-8") as file:
+        books = json.load(file)
+        new_books = [b for b in books if b["id"] != book_id]
+
+    with open("books.json", "w", encoding="utf-8") as file:
+        json.dump(new_books, file)
+
+    return Message(message="Book deleted successfully")
